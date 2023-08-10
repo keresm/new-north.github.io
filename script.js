e27ca26d43741ddce7e4e3e1df93778380aa30df
Aiyyna <49439078+keresm@users.noreply.github.com>
2023-08-10 15:39:22 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Пример: изменение цвета заголовка при клике
+const header = document.querySelector('header');
+
+header.addEventListener('click', () => {
+    header.style.backgroundColor = 'blue';
+});
